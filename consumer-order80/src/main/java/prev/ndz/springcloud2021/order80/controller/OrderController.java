c362112252934a47a4d57b198a3c100fe4fc1938
nidazhong <nidazhong17@163.com>
2023-01-25 19:00:55 +0800

    first commit

--- consumer-order80/src/main/java/prev/ndz/springcloud2021/order80/controller/OrderController.java
+++ consumer-order80/src/main/java/prev/ndz/springcloud2021/order80/controller/OrderController.java
@@ -3,6 +3,7 @@ package prev.ndz.springcloud2021.order80.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.beans.BeanCopier;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.ResponseEntity;
---
+++ my-utils/README.md
@@ -0,0 +1,8 @@
+
+
+Easy-mapper 手册
+http://neoremind.com/2016/08/easy-mapper-%E4%B8%80%E4%B8%AA%E7%81%B5%E6%B4%BB%E5%8F%AF%E6%89%A9%E5%B1%95%E7%9A%84%E9%AB%98%E6%80%A7%E8%83%BDbean-mapping%E7%B1%BB%E5%BA%93/
+
+
+fastbean， 基于cglib
+https://github.com/cqpsjsl/fastbean
